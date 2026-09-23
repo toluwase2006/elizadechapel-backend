46bb343315e97e51f503881f4788d7e19e9ef3b8
toluwase2006 <08067600639te@gmail.com>
2026-09-23 18:00:59 +0100

    adjusted cors policy

--- api/index.js
+++ api/index.js
@@ -108,7 +108,7 @@ async function connectDB() {
 if (require.main === module) {
   const port = Number(process.env.PORT) || 5000;
 
-  app.listen(port, () => {
+  app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on port ${port}`);
   });
 
